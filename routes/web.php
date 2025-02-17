e550aeab7543945acc032d02009fdd2f377030e1
David Hype <iamsocialhype@gmail.com>
2025-02-17 23:17:04 +0100

    chore

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 
-Route::apiResource('todo', TodoController::class);
+// Route::apiResource('todo', TodoController::class);
+Route::get("todo", [TodoController::class, "index" ]);
+Route::post("todo", [TodoController::class, "store"]);
